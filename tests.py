0bde9dd6322d6a9d45b8e6b6d443dc0ef47def98
kytran <kytran@sfu.ca>
2018-05-13 17:11:08 -0700

    add spl-004 test case

--- tests.py
+++ tests.py
@@ -6,7 +6,29 @@ import json
 class TestURL(unittest.TestCase):
 
     url = 'https://splunk.mocklab.io/movies'
+    drop_box_url = "https://api.dropboxapi.com/2/files/get_metadata"
+    max_genre_ids = 400
+    max_genre_ids_count = 7
 
+    # def test_print(self):
+    #     """
+    #     SPL-005:
+    #     There is at least one movie in the database whose title has a palindrome in it.
+    #     """
+    #     r = self._get_movies(movie='batman')
+    #     print(self._validate_drop_box_link('https://www.dropbox.com/s/8i8v4ak8tnp03w4/'))
+
+    def test_spl_004(self):
+        """
+        SPL-004:
+        The number of movies whose sum of "genre_ids" < 400 should be no more than 7.
+        """
+        r = self._get_movies(movie='batman')
+        count = 0
+        for movie in r.json()['results']:
+            if sum(movie['genre_ids']) > self.max_genre_ids:
+                count += 1
+        self.assertTrue(count <= self.max_genre_ids_count, 'Exceeded max count')
 
     def test_spl_005(self):
         """
@@ -30,6 +52,21 @@ class TestURL(unittest.TestCase):
         params = {'q': movie}
         headers = {'accept': 'application/json'}
         return requests.post(self.url, data=json.dumps(params))
+
+    def _validate_drop_box_link(self, url):
+        headers = {
+            "Authorization": "Bearer null",
+            "Content-Type": "application/json",
+            "Dropbox-Api-Select-User": ""
+        }
+        data = {
+            "path": url
+        }
+        if url is None:
+            return True
+        else:
+            r = requests.post(self.drop_box_url, headers=headers, data=json.dumps(data))
+            return r.status_code
 
     @staticmethod
     def _is_palindrome(s):
